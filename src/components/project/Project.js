da87bc1f576b09f3af9f23cb3ef2d17a5d20e876
trisdauvergne <tristan.dauvergne@gmail.com>
2022-01-26 11:32:46 +0100

    Developed grid structure in project component

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -5,17 +5,23 @@ const Project = ({ project }) => {
     console.log('single file', project)
     return (
         <div className="project">
-            <h2>{project.projectTitle}</h2>
-            <p>{project.projectIntro}</p>
-            <p><strong>Read more +</strong></p>
-            <div>
+            <div className="project__title">
+                <h2>{project.projectTitle}</h2>
+                <p>{project.projectIntro}</p>
+            </div>
+            <div className="project__copy">
+                <p><strong>Read more +</strong></p>
                 <p>{project.projectBody.projectBody}</p>
                 <p>{project.projectCreative.projectCreative}</p>
             </div>
-            <p><strong>Tools & languages +</strong></p>
-            {project.toolsAndLanguages.map((tool, i) => <p key={i}>{tool}</p>)}
-            <p>{project.deployedLink}</p>
-            <p>{project.sourceCode}</p>
+            <div className="project__tools">
+                <p><strong>Tools & languages +</strong></p>
+                {project.toolsAndLanguages.map((tool, i) => <p key={i}>{tool}</p>)}
+            </div>
+            <div className="project__links">
+                <p>{project.deployedLink}</p>
+                <p>{project.sourceCode}</p>
+            </div>
         </div>
     )
 }
