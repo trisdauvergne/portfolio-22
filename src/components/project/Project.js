4ebcd44d77cb80ae11d07f6b028ebb50d8b162d4
trisdauvergne <tristan.dauvergne@gmail.com>
2022-01-26 12:02:03 +0100

    Added useState in project component for displaying body and tools

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './project.scss';
 
 const Project = ({ project }) => {
+    const [ visibleBody, setVisibleBody ] = useState(false);
+    const [ visibleTools, setVisibleTools ] = useState(false);
+
     console.log('single file', project)
+
+    const changeBodyVisibility = () => {
+        setVisibleBody(!visibleBody);
+        setVisibleTools(false);
+    };
+
+    const changeToolsVisibility = () => {
+        setVisibleTools(!visibleTools);
+        setVisibleBody(false);
+    };
+    
     return (
         <div className="project">
             <div className="project__title">
                 <h2>{project.projectTitle}</h2>
                 <p>{project.projectIntro}</p>
             </div>
-            <div className="project__copy">
+            <div className="project__btns">
+                <button onClick={changeBodyVisibility}>Read more +</button>
+                <button onClick={changeToolsVisibility}>Tools & Languages +</button>
+                <p>{project.deployedLink}</p>
+                <p>{project.sourceCode}</p>
+            </div>
+            <div className="project__info">
+                {visibleBody && <div>
+                    <p>{project.projectBody.projectBody}</p>
+                    <p>{project.projectCreative.projectCreative}</p>
+                </div>}
+                {visibleTools && <div>
+                    {project.toolsAndLanguages.map((tool, i) => <p key={i}>{tool}</p>)}
+                </div>}
+            </div>
+            {/* <div className="project__copy">
                 <p><strong>Read more +</strong></p>
                 <p>{project.projectBody.projectBody}</p>
                 <p>{project.projectCreative.projectCreative}</p>
@@ -21,7 +50,7 @@ const Project = ({ project }) => {
             <div className="project__links">
                 <p>{project.deployedLink}</p>
                 <p>{project.sourceCode}</p>
-            </div>
+            </div> */}
         </div>
     )
 }
