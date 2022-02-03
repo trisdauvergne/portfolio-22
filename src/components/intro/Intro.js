14465722294d5e11d96778d715b6160d49272215
trisdauvergne <tristan.dauvergne@gmail.com>
2022-02-03 17:12:56 +0100

    Testing framer motion works after deployed to netlify

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import { motion } from 'framer-motion';
 import './intro.scss';
 
 const Intro = () => {
     return (
         <section className="intro">
-            <h1>TRISTAN DAUVERGNE</h1>
+            <motion.h1
+                animate={{ scale: 2 }}
+                transition={{ duration: 0.5 }}>TRISTAN DAUVERGNE</motion.h1>
             <Link to="about" smooth={true} duration={1000}>
                 <button className="nav-btn">Scroll down</button>
             </Link>
