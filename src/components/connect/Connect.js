c2c40bdeac7a2c98d85eb154aed703866cd88111
trisdauvergne <tristan.dauvergne@gmail.com>
2022-02-03 16:33:07 +0100

    Removed arrows

--- src/components/connect/Connect.js
+++ src/components/connect/Connect.js
@@ -7,9 +7,10 @@ const Connect = () => {
         <section className="connect" id="connect">
             <h1 className="section-heading">Connect.</h1>
             <div className="connect-btns">
-                <a href="https://www.linkedin.com/in/tristandauvergne/" target="_blank" rel="noreferrer">LinkedIn<span className="material-icons lrg-icon">arrow_right_alt</span></a>
-                <a href="https://github.com/trisdauvergne" target="_blank" rel="noreferrer">GitHub<span className="material-icons lrg-icon">arrow_right_alt</span></a>
-                <a href="mailto:tristan.dauvergne@gmail.com" target="_blank" rel="noreferrer">Email<span className="material-icons lrg-icon">arrow_right_alt</span></a>
+                <a href="https://www.linkedin.com/in/tristandauvergne/" target="_blank" rel="noreferrer">LinkedIn</a>
+                <a href="https://github.com/trisdauvergne" target="_blank" rel="noreferrer">GitHub</a>
+                <a href="mailto:tristan.dauvergne@gmail.com" target="_blank" rel="noreferrer">Email</a>
+                {/* <a href="mailto:tristan.dauvergne@gmail.com" target="_blank" rel="noreferrer">Email<span className="material-icons lrg-icon">arrow_right_alt</span></a> */}
             </div>
             <Link to="about" smooth={true} duration={1000}>
                 <button className="nav-btn">Back to top</button>
